58572d30719ba812ea583243ed6c9b0e1dd44887
BigGuyAlex <84516641+BigGuyAlex@users.noreply.github.com>
2021-06-10 15:33:51 +0000

    afae

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ function checkDiscount () {
   age = document.getElementById('age').value
   day = document.getElementById('day').value
 
-// check if requirements met
-   if ((day.toLowerCase() === 'tuesday' || day.toLowerCase() === 'thursday') && (age > 12 && age < 21)) {
-      document.getElementById('answer').innerHTML = 'You can get a discount'
-  } 
-  else {
+  // check if requirements met
+  if ((day.toLowerCase() === 'tuesday' || day.toLowerCase() === 'thursday') && (age > 12 && age < 21)) {
+    document.getElementById('answer').innerHTML = 'You can get a discount'
+  } else {
     document.getElementById('answer').innerHTML = 'You cannot get a discount'
-  } 
-} 
+  }
+}
